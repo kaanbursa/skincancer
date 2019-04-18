ae677effe6fa15b5714fbd9d3556429e2aaaefe3
kaan bursa <kaanbursa9@gmail.com>
2019-04-18 13:37:10 +0300

    css and model

--- jscript/predict202.js
+++ jscript/predict202.js
@@ -45,7 +45,7 @@ $("#image-selector").change(function () {
 let model;
 (async function () {
 	
-	model = await tf.loadModel('https://github.com/vbookshelf/Skin-Lesion-Analyzer/blob/master/final_model_kaggle_version1/model.json');
+	model = await tf.loadModel('https://github.com/kaanbursa/skincancer/blob/master/final_model_kaggle_version1/model.json');
 	$("#selected-image").attr("src", "http://skin.test.woza.work/assets/samplepic.jpg")
 	
 	
